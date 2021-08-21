edd50b97bd9a795e2b088f30488e2f7959f0d6f7
eaaniwezi <eaaninwezi@gmail.com>
2021-08-21 18:33:09 +0300

    google sign in

--- lib/providers/users_providers/user_provider.dart
+++ lib/providers/users_providers/user_provider.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ecommerce_app/services/user_services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ecommerce_app/services/user_services.dart';
 
 enum Status {
   Uninitialized,
